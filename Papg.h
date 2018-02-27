461963fc2283bdff4e7699c95f470c132511b8d6
Marco Antonio Rosa da silva <marco.silva@unochapeco.edu.br>
2018-02-27 11:41:38 -0300

    Add main e papg
    Adicionei o main mas não coloquei nada util, também comecei o papg.h

---
+++ Papg.h
@@ -0,0 +1,18 @@
+#ifndef PAPG_H_INCLUDED
+#define PAPG_H_INCLUDED
+
+class Pa{
+public:
+    vector <float> numeros;
+    float aum;
+    float an;
+    float razao;
+    float numeroTermos;
+    float somaTermos;
+
+    int setNumeros ()
+
+private:
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "Papg.h"
+
+using namespace std;
+
+int main(){
+    cout << "Ola mundo" << endl;
+    return 0;
+}
